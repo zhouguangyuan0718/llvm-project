03a50162713e38093ad6b8587e0b82b0230f80e8
zhouguangyuan0718 <zhouguangyuan.xian@gmail.com>
2026-07-16 11:09:57 +0800

    Add generic memref reduction pass header

---
+++ llvm/include/llvm/Transforms/Scalar/LowerMemRefReductionToScalar.h
@@ -0,0 +1,24 @@
+//===- LowerMemRefReductionToScalar.h - Lower reductions --------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_TRANSFORMS_SCALAR_LOWERMEMREFREDUCTIONTOSCALAR_H
+#define LLVM_TRANSFORMS_SCALAR_LOWERMEMREFREDUCTIONTOSCALAR_H
+
+#include "llvm/IR/PassManager.h"
+
+namespace llvm {
+
+class LowerMemRefReductionToScalarPass
+    : public PassInfoMixin<LowerMemRefReductionToScalarPass> {
+public:
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
+};
+
+} // namespace llvm
+
+#endif // LLVM_TRANSFORMS_SCALAR_LOWERMEMREFREDUCTIONTOSCALAR_H
